bf3dc46875fa8d74593e1e9f02e9f1622b911e05
Adeeb Misbahi <130211733+AdeebMisbahi1@users.noreply.github.com>
2023-05-15 19:32:42 +0530

    Update rootToLeafPathsSumToK.java
    For a given Binary Tree of type integer and a number K, print out all root-to-leaf paths where the sum of all the node data along the path is equal to K.

--- BinaryTrees/Assignments/rootToLeafPathsSumToK.java
+++ BinaryTrees/Assignments/rootToLeafPathsSumToK.java
@@ -1,3 +1,16 @@
+
+//     /*	Binary Tree Node class
+//  * 
+//  * class BinaryTreeNode<T> {
+// 		T data;
+// 		BinaryTreeNode<T> left;
+// 		BinaryTreeNode<T> right;
+
+// 		public BinaryTreeNode(T data) {
+// 			this.data = data;
+// 		}
+// 	}
+// 	*/
 public class Solution {
   private static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k, String path, int currSum) { 
     if(root == null) {
@@ -16,4 +29,63 @@ public class Solution {
   public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
     rootToLeafPathsSumToK(root, k, "", 0);
   }
+  
+  
+  
+//   OR
+  
+//   public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
+// 	       // Write your code here
+//         helper(root,k,"");
+//     }
+//     private static void helper(BinaryTreeNode<Integer> root,int k,String s){
+//         if(root==null)
+//             return;
+//         if(root.left==null && root.right==null && root.data==k)
+//         {
+//             System.out.println(s+root.data);
+//             return;
+//         }
+//         s=s+root.data+" ";
+   
+//         helper(root.left,k-root.data,s);
+//         helper(root.right,k-root.data,s);
+        
+//     }  
+  
+  
+//  OR
+  //     public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k) {
+// 		rootToLeafPathsSumToK(root,k,"");
+// 	}
+  // 	public static void rootToLeafPathsSumToK(BinaryTreeNode<Integer> root, int k,String ans) {
+//           if(k==root.data){      
+//               if(root.right==null&&root.left==null){
+//                  System.out.println(ans+" "+root.data);
+//                  return; 
+//               }else{
+//                   return;
+//               }
+//           }
+         
+//         if(root.left!=null){
+//           if(ans=="")
+//             rootToLeafPathsSumToK(root.left ,k-root.data,ans+root.data);
+//            else{ 
+//             rootToLeafPathsSumToK(root.left ,k-root.data,ans+" "+root.data);
+//            }
+//         }
+//         if(root.right!=null){  
+//             if(ans=="")
+//             rootToLeafPathsSumToK(root.right ,k-root.data,ans+root.data);
+//            else{ 
+//             rootToLeafPathsSumToK(root.right ,k-root.data,ans+" "+root.data);
+//            }
+//         }
+//     }
+  
+  
+  
+  
+  
 }
